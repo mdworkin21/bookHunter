45281310cd76b2702ebfc11c85a2d5a4d02b6c11
Matt Dworkin <mdworkin21@gmail.com>
2019-01-03 18:48:19 -0500

    some basic set up for backend

---
+++ server/db/database.js
@@ -0,0 +1,7 @@
+const Sequelize = require('sequelize')
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/bookHunter', {logging: true})
+
+//This file creates the db connection with postgres. Models are defined in their own folders.
+
+module.exports = db
+
---
+++ server/db/models/index.js
@@ -0,0 +1,12 @@
+const db = require('./database')
+
+const Sequelize = require('sequelize')
+
+//Define your associations here
+
+
+//export models
+module.exports = {
+  db,
+  
+}
