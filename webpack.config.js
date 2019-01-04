f9650ac5a559181eb14bee6713ec66af7d4371f9
Matt Dworkin <mdworkin21@gmail.com>
2019-01-04 09:47:25 -0500

    heroku bundle prob

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,9 @@
-// module.exports = {
-//   entry: './client', // assumes your entry point is the index.js in the root of your project folder
-//   mode: 'development',
-//   output: {
-//     path: __dirname + '/client/public', 
-//     filename: 'bundle.js',
-//     publicPath: '/'
-//   },
-//   devtool: 'source-maps',
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader'
-//         }
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [
-//           'style-loader',
-//           'css-loader',
-//         ]
-//       }
-//     ]
-//   }
-// }
-
 module.exports = {
-  entry: './client/', // assumes your entry point is the index.js in the root of your project folder
+  entry: './client', // assumes your entry point is the index.js in the root of your project folder
   mode: 'development',
   output: {
-    path: __dirname  + '/client/public', // assumes your bundle.js will also be in the root of your project folder
-    filename: 'bundle.js'
+    path: __dirname + '/client/public', 
+    filename: 'bundle.js',
   },
   devtool: 'source-maps',
   module: {
@@ -54,3 +25,4 @@ module.exports = {
     ]
   }
 }
+
