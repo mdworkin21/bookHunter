f27ed08856c90dcaac4f5cc76fd240d0b0af69bd
Matt Dworkin <mdworkin21@gmail.com>
2019-01-04 14:55:34 -0500

    search bar with some styling

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import SearchBar from './SearchBar';
 
 const App = () => {
   return (
-  <h1>Book Hunter</h1>
+  <div>
+    <h1>Book Hunter</h1>
+    <div className="ui container" style={{marginTop: '10em'}}>
+      <SearchBar />
+    </div>
+  </div>
   )
 }
 
---
+++ client/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../public/style/SearchBar.css'
+
+const SearchBar = (props) => {
+  return (
+    <div className="ui segment">
+      <form className="ui form searchBar">
+        <div className="field">
+          <label>Book Seach</label>
+          <input type="text" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SearchBar
