b4257efbef56df7b34ca1beea0da5360138d04ee
Matt Dworkin <mdworkin21@gmail.com>
2019-01-06 16:10:50 -0500

    query string bug fix

--- server/api/openLibrary.js
+++ server/api/openLibrary.js
@@ -4,8 +4,17 @@ const axios = require('axios')
             
 router.get('/:search?', async (req, res, next) => {
   try{
-    let queryString = req.query.q !== undefined ? `q=${req.query.q}`: `author=${req.query.author}&title=${req.query.title}`
-      let response = await axios.get(`http://openlibrary.org/search.json?${queryString}`) 
+   let advanced;
+   if (!req.query.title){
+     advanced = `author=${req.query.author}`
+   } else if (!req.query.author){
+      advanced = `title=${req.query.title}`
+   } else {
+      advanced = `author=${req.query.author}&title=${req.query.title}`
+   }
+
+    let queryString = req.query.q !== undefined ? `q=${req.query.q}`: advanced
+    let response = await axios.get(`http://openlibrary.org/search.json?${queryString}`) 
           res.status(200).json(response.data)
     } catch(err){
         next(err)
