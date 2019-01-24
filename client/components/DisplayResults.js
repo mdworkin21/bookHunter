b5a6ebafef87a41d9e3f57963df613e3b9b0c040
Matt Dworkin <mdworkin21@gmail.com>
2019-01-24 16:47:06 -0500

    cleaned up pagination code a little, still needs styling but functionality works

--- client/components/DisplayResults.js
+++ client/components/DisplayResults.js
@@ -4,40 +4,6 @@ import '../public/style/DisplayResults.css'
 import {Link} from 'react-router-dom'
 import PaginateBtn from './PaginateBtn';
 
-// const DisplayResults = (props) => {
-//   let results = props.state.results
-//   let start = 0
-//   let end = start + 10
-//   let currentResults = results.slice(start, end)
-  
-//   return (
-//     <React.Fragment>
-//     {currentResults.map((el, index) =>{
-//       let isbnNum = el.isbn ? el.isbn[0] : ""
-//       let displayImage = isbnNum === "" ? "openBook.jpg" :
-//       `https://covers.openlibrary.org/b/isbn/${isbnNum}-M.jpg`
-      
-//       return(
-//         <div className="ui card" id="display-results-container" key={index}>
-//           <div className="image display-results-child" id="child-image">
-//             <img src={displayImage}/>
-//           </div>
-//           <div className="content" id="child-content">
-//             <Link to={`/details/${index}`} className="header">{el.title_suggest}</Link>
-//             <p>{el.subtitle}</p>
-//             <div className="meta">
-//               <span className="date display-results-child">By: {el.author_name}</span>
-//             </div>
-//             <div className="description display-results-child">Published: {el.first_publish_year}</div>
-//           </div>
-//         </div>
-//         )
-//       })}
-//     </React.Fragment>
-//   )
-// }
-
-
 class DisplayResults extends Component {
   state = {
     start: 0,
@@ -58,11 +24,8 @@ class DisplayResults extends Component {
     }
   }
 
-
-
   render(){
   let results = this.props.state.results
-  
   let currentResults = results.slice(this.state.start, this.state.end)
   
   return (
