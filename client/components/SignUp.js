24cc45228ad8e49d6f92450193031d436f08e1ab
Matt Dworkin <mdworkin21@gmail.com>
2019-01-30 14:48:32 -0500

    basic sign up form with css

--- client/components/SignUp.js
+++ client/components/SignUp.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react'
-import { createNewUser } from '../redux/store'
+import { createNewUser } from '../redux/thunks/users'
 import {connect} from 'react-redux'
 import regeneratorRuntime, { async } from "regenerator-runtime";
-
+import '../public/style/SignUp.css'
 
 
 class SignUp extends Component {
   state = {
-    name: '',
+    userName: '',
     email: '',
-    password: ''
+    password: '',
+    repassword: ''
   }
 
   handleChange = (event) => {
@@ -22,14 +23,15 @@ class SignUp extends Component {
     event.preventDefault()
     try{
      await this.props.createUser({
-        name: this.state.name,
+        userName: this.state.userName,
         email: this.state.email,
         password: this.state.password
       })
       this.setState({
-        name: '',
+        userName: '',
         email: '',
-        password: ''
+        password: '',
+        repassword: ''
       }) 
     } catch(err){
         console.log(err)
@@ -38,22 +40,34 @@ class SignUp extends Component {
 
   //Just a draft of styling
   render(){
+    // return(
+    //   <form className="ui form" onSubmit={this.handleSubmit}>
+    //     <div className="field">
+    //       <label>Name</label>
+    //       <input type="text" name="name" placeholder="Name" onChange={this.handleChange} value={this.state.name}/>
+    //     </div>
+    //     <div className="field">
+    //       <label>Email</label>
+    //       <input type="text" name="email" placeholder="Email" onChange={this.handleChange} value={this.state.email}/>
+    //     </div>
+    //     <div className="field">
+    //       <label>Password</label>
+    //       <input type="text" name="password" placeholder="Password" onChange={this.handleChange} value={this.state.password}/>
+    //     </div>
+    //     <button className="ui button" type="submit">Submit</button>
+    //   </form>
+    // )
     return(
-      <form className="ui form" onSubmit={this.handleSubmit}>
-        <div className="field">
-          <label>Name</label>
-          <input type="text" name="name" placeholder="Name" onChange={this.handleChange} value={this.state.name}/>
+      <div id="login-box">
+        <div className="left-box" onSubmit={this.handleSubmit}>
+          <h1>Sign Up</h1>
+          <input type="text" name="userName" placeholder="Username" onChange={this.handleChange} value={this.state.userName} />
+          <input type="text" name="email" placeholder="Email" onChange={this.handleChange} value={this.state.email} />
+          <input type="password" name="password" placeholder="Password" onChange={this.handleChange} value={this.state.password} />
+          <input type="password" name="repassword" placeholder="Retype Password" onChange={this.handleChange} value={this.state.repassword} />
+          <input type="submit" name="signup-btn" value="Sign Up" onClick={this.handleSubmit}/>
         </div>
-        <div className="field">
-          <label>Email</label>
-          <input type="text" name="email" placeholder="Email" onChange={this.handleChange} value={this.state.email}/>
-        </div>
-        <div className="field">
-          <label>Password</label>
-          <input type="text" name="password" placeholder="Password" onChange={this.handleChange} value={this.state.password}/>
-        </div>
-        <button className="ui button" type="submit">Submit</button>
-      </form>
+      </div>
     )
   }
 }
