d97f24abbde319dbad07dd97fd09f7fc26289975
Matt Dworkin <mdworkin21@gmail.com>
2019-02-04 13:23:55 -0500

    menu display changes based on signin

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -10,17 +10,34 @@ class Menu extends Component  {
     return <Redirect to='/' />
   }
 
+  menuList(){
+    if (!this.props.user.userName){
+      return (
+        <React.Fragment>
+          <NavLink to='/' id="menu-links">Home</NavLink>
+            <br/>
+         <NavLink to='/signup' id="menu-links">Sign Up/ Log In</NavLink>
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <React.Fragment>
+          <h2>Welcome, {this.props.user.userName}</h2>
+          <NavLink to='/' id="menu-links">Home</NavLink>
+          <br/> 
+          <NavLink to='/profile' id="menu-links">Profile</NavLink>
+          <div id="menu-links" onClick={this.handleClick}>Log Out</div>
+        </React.Fragment>
+      )
+    }
+  }
+
   render(){
+    let menuDisplay = this.menuList()
     return (
       <div className="ui vertical menu" id="menu-container">
         <h1>Book Hunter</h1>  
-        <h2>{this.props.user.userName}</h2> 
-          <NavLink to='/' id="menu-links">Home</NavLink>
-          <br/>
-          <NavLink to='/signup' id="menu-links">Sign Up/ Log In</NavLink>
-          <br/>
-          <NavLink to='/profile' id="menu-links">Profile</NavLink>
-          <div id="menu-links" onClick={this.handleClick}>Log Out</div>
+          {menuDisplay}
       </div>
     )
 
