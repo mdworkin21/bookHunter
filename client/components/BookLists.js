04615f12e9ee817310efd8a3c006162aac2e6c3a
Matt Dworkin <mdworkin21@gmail.com>
2019-02-06 15:16:41 -0500

    book lists slightly styled

--- client/components/BookLists.js
+++ client/components/BookLists.js
@@ -3,18 +3,26 @@ import '../public/style/BookLists.css'
 
 const BookLists = (props) => {
   return (
-    <div className="ui segments list-container">
+    <div className="ui items list-container">
       <div className="ui segment">
         <h1>{props.listType}</h1>
       </div>
       {props.list.map(item => {
-        return (
-          <React.Fragment>
-            <div className="ui segment">
-              {item.id + '.' + ' '} 
-              {item.title} 
-            </div> 
-          </React.Fragment>
+        return ( 
+          <div className="ui item single-item" key={item.id}>          
+            <div className="image">
+              <img src="openBook.jpg" />
+            </div>
+            <div className="content single-item">
+              <a className="header">Title: {item.title}</a>
+              <div className="meta single-item">
+                <span>By: {item.author}</span>
+              </div>
+              <div className="extra single-item">
+                Year: {item.publishYear}
+              </div>
+            </div>
+          </div>         
         )
       })}
     </div>
